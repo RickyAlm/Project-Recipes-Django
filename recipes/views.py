897595fd696424d53cb767e335f72ea57155e034
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-08-04 20:56:19 -0300

    add data dynamically to recipes with faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    context = {
+        'recipes': [make_recipe() for _ in range(10)]
+    }
+
+    return render(request, 'recipes/pages/home.html', context=context)
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    context = {
+        'recipe': make_recipe()
+    }
+
+    return render(request, 'recipes/pages/recipe-view.html', context=context)
