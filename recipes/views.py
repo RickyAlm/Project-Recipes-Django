c071bc9165bcd3fc901bc58ed0dc2f54e6e2b22b
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-08-04 22:29:10 -0300

    edit the content of the single card and change the URLs

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     context = {
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': [make_recipe() for _ in range(10)],
     }
 
     return render(request, 'recipes/pages/home.html', context=context)
@@ -13,7 +13,8 @@ def home(request):
 
 def recipe(request, id):
     context = {
-        'recipe': make_recipe()
+        'recipe': make_recipe(),
+        'is_detail_page': True
     }
 
     return render(request, 'recipes/pages/recipe-view.html', context=context)
