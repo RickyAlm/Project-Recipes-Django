259ac5e4dd5dd9fee56717b8d7fb40097cda21c0
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-08-10 00:22:43 -0300

    apply migration to the database

--- recipes/models.py
+++ recipes/models.py
@@ -21,5 +21,6 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
     category = models.ForeignKey(
-    Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True
+    )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
