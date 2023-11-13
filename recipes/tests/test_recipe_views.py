672bfd08de283cf7e9d95d0a934bd7166fd3a726
Henrique Almeida <rick.vieira.almeida@gmail.com>
2023-11-13 18:38:08 -0300

    add 404 page for an empty search query

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -131,5 +131,10 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertEqual(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search'))
+        # Need a search field
+        response = self.client.get(reverse('recipes:search') + '?q=test')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertEqual(response.status_code, 404)
