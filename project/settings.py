32bab5fde2aab96f99deef5185e4bf3b565fd198
Henrique Almeida <rick.vieira.almeida@gmail.com>
2024-01-26 20:48:35 -0300

    add configuration and css for flash messages

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +137,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.SUCCESS: 'message-success',
+    constants.ERROR: 'message-error',
+    constants.WARNING: 'message-warning',
+    constants.INFO: 'message-info',
+    constants.DEBUG: 'message-debug',
+}
 
 SUMMERNOTE_CONFIG = {
     'summernote': {
